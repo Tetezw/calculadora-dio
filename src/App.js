3123191ce162287adfbe1425ae0287b98627f288
Tetezw <terezawisser@gmail.com>
2024-05-12 21:14:18 -0300

    feat: calculadora

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import { Container, Content, Row } from "./styles";
 
-function App() {
+const App = () => {
+  const [numeroAtual, setNumeroAtual] = useState("0");
+  const [primeiroNumero, setPrimeiroNumero] = useState("0");
+  const [operacao, setOperacao] = useState("");
+
+  const lidandoAdicionarNumero = (numero) => {
+    setNumeroAtual(
+      (anterior) => `${anterior === "0" ? "" : anterior}${numero}`
+    );
+  };
+
+  const limparNumeros = () => {
+    setNumeroAtual("0");
+    setPrimeiroNumero("0");
+    setOperacao("");
+  };
+
+  const lidandoSomaNumeros = () => {
+    if (primeiroNumero === "0") {
+      setPrimeiroNumero(String(numeroAtual));
+      setNumeroAtual("0");
+      setOperacao("+");
+    } else {
+      const soma = Number(primeiroNumero) + Number(numeroAtual);
+      setNumeroAtual(String(soma));
+      setOperacao("");
+    }
+  };
+
+  const lidandoIgual = () => {
+    if (primeiroNumero !== "0" && operacao !== "" && numeroAtual !== "0") {
+      switch (operacao) {
+        case "+":
+          lidandoSomaNumeros();
+          break;
+        case "-":
+          lidandoSubtracaoNumeros();
+          break;
+        case "/":
+          lidandoDivisaoNumeros();
+          break;
+        case "x":
+          lidandoMultiplicacaoNumeros();
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  const lidandoSubtracaoNumeros = () => {
+    if (primeiroNumero === "0") {
+      setPrimeiroNumero(String(numeroAtual));
+      setNumeroAtual("0");
+      setOperacao("-");
+    } else {
+      const subtracao = Number(primeiroNumero) - Number(numeroAtual);
+      setNumeroAtual(String(subtracao));
+      setOperacao("");
+    }
+  };
+
+  const lidandoDivisaoNumeros = () => {
+    if (primeiroNumero === "0") {
+      setPrimeiroNumero(String(numeroAtual));
+      setNumeroAtual("0");
+      setOperacao("/");
+    } else {
+      const subtracao = Number(primeiroNumero) / Number(numeroAtual);
+      setNumeroAtual(String(subtracao));
+      setOperacao("");
+    }
+  };
+
+  const lidandoMultiplicacaoNumeros = () => {
+    if (primeiroNumero === "0") {
+      setPrimeiroNumero(String(numeroAtual));
+      setNumeroAtual("0");
+      setOperacao("x");
+    } else {
+      const subtracao = Number(primeiroNumero) * Number(numeroAtual);
+      setNumeroAtual(String(subtracao));
+      setOperacao("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={numeroAtual} />
+        <Row>
+          <Button label="x" onClick={lidandoMultiplicacaoNumeros} />
+          <Button label="/" onClick={lidandoDivisaoNumeros} />
+          <Button label="c" onClick={limparNumeros} />
+          <Button label="on" />
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => lidandoAdicionarNumero("7")} />
+          <Button label="8" onClick={() => lidandoAdicionarNumero("8")} />
+          <Button label="9" onClick={() => lidandoAdicionarNumero("9")} />
+          <Button label="-" onClick={lidandoSubtracaoNumeros} />
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => lidandoAdicionarNumero("4")} />
+          <Button label="5" onClick={() => lidandoAdicionarNumero("5")} />
+          <Button label="6" onClick={() => lidandoAdicionarNumero("6")} />
+          <Button label="+" onClick={lidandoSomaNumeros} />
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => lidandoAdicionarNumero("1")} />
+          <Button label="2" onClick={() => lidandoAdicionarNumero("2")} />
+          <Button label="3" onClick={() => lidandoAdicionarNumero("3")} />
+          <Button label="=" onClick={lidandoIgual} />
+        </Row>
+      </Content>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #b4b2bd;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size: 24px;
+  font-family: "Roboto";
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #b4b2bd;
+    border: 1px solid #7f80c4;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 0 10px;
+    font-size: 24px;
+    font-family: "Roboto";
+    color: #000000;
+  }
+`;
